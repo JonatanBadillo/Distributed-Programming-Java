3db72442e763b618f6b54bf848241f8a7497efb7
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-05 17:02:06 -0600

    Refactorizar clase Main para mejorar legibilidad del código

--- Tarea POO/Programa 2/main.java
+++ Tarea POO/Programa 2/main.java
@@ -1,5 +1,5 @@
-
-
+// Escribe un pequeño programa utilizando
+// las dos clases Punto y PuntoA.
 class Main {
     public static void main(String[] args) {
         int x = 5;
@@ -17,3 +17,8 @@ class Main {
         
     }
 }
+
+// ¿Qué pasaría si la clase Punto no tuviera métodos GetX y Gety?
+// No podría acceder directamente a los valores de X y Y fuera de la clase, ya que estas variables son privadas (private int x, y;).
+// Para acceder a estos valores deberia cambiar la visibilidad de X y Y a public, lo cual rompería el principio de encapsulación:
+// Las variables de instancia, X y Y, se declaran como privadas para ocultarlas directamente del código externo a la clase.
