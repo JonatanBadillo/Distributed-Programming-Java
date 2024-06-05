262e6f31e325e435c80086a1117298c446b39ecb
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-05 17:28:50 -0600

    chore: Eliminar clases Punto y PuntoA y refactorizar clase Main

--- Tarea POO/Programa 2/PuntoA.java
+++
@@ -1,9 +0,0 @@
-// Proponer una clase PuntoA, derivada de Punto con un método visualiza que muestra (en la
-// ventana de la consola) las coordenadas de un punto. Escribe un pequeño programa utilizando
-// las dos clases Punto y PuntoA.
-
-public class PuntoA extends Punto{
-    public void visualiza(){
-        System.out.println("Coordenadas: (" + getX() + ", " + getY() + ")");
-    }
-}
