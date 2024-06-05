d9988c3fe6ee4c383b20dbb539a65791cb03adc0
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-05 16:48:43 -0600

    feat: Agregar clase PuntoA derivada de Punto con método visualiza

--- Tarea POO/Programa 2/Punto.java
+++ Tarea POO/Programa 2/Punto.java
@@ -1,10 +1,16 @@
+// Sea la siguiente clase Punto:
 
 class Punto {
+
+    // Establecer los valores de las coordenadas x e y
     public void inicializa(int x, int y) {
+        // this.x y this.y son las variables de instancia x e y de la clase Punto.
         this.x = x;
         this.y = y;
     }
 
+
+     //Desplaza el punto en las coordenadas x e y.
     public void desplaza(int dx, int dy) {
         x += dx;
         y += dy;
@@ -20,3 +26,8 @@ class Punto {
 
     private int x, y;
 }
+
+
+// Proponer una clase PuntoA, derivada de Punto con un método visualiza que muestra (en la
+// ventana de la consola) las coordenadas de un punto. Escribe un pequeño programa utilizando
+// las dos clases Punto y PuntoA.
