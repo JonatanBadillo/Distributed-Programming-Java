937c7e335f20356c32b53ecc255af8c7c8aafeb4
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-05 11:05:26 -0600

    estructura de la tarea de poo

---
+++ Tarea POO/programa3.java
@@ -0,0 +1,7 @@
+
+
+public class programa3 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Tarea POO/programa4.java
@@ -0,0 +1,6 @@
+
+public class programa4 {
+    public static void main(String[] args) {
+        System.err.println("holaa");
+    }
+}
