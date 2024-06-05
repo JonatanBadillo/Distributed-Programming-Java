fea9749158e4991e708ba4b348ebcee83e676cf9
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-05 17:54:38 -0600

    Refactorizar clase Punto eliminada y refactorizar clase Main

---
+++ Tarea POO/Programa 3/Main.java
@@ -0,0 +1,39 @@
+// Sea la siguiente clase:
+class Punto {
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void visualiza() {
+        System.out.println("Coordenadas : " + x + " " + y);
+    }
+
+    private int x, y;
+}
+
+// Propone una clase PuntoName, derivada de Punto, que te permita manipular puntos definidos
+// por sus coordenadas y un nombre (carácter). Se proporcionarán los siguientes métodos:
+//         • constructor para definir las coordenadas y el nombre de un objeto de tipo PuntoName
+//         • visualiza para mostrar las coordenadas y el nombre de un objeto de tipo PuntoName.
+
+
+class PuntoName extends Punto{
+    private String name;
+    // Constructor para definir las coordenadas y el nombre 
+    public PuntoName(int x, int y, String name){
+        super(x, y);// Llamada al constructor de la clase padre
+        this.name = name;
+    }
+
+    // Método visualiza que muestra las coordenadas del punto y su nombre
+    @Override
+    public void visualiza(){
+        super.visualiza();  // Llamar al método visualiza de la clase padre
+        System.out.println("Nombre : " + name);
+    }
+}
+
+public class Main {
+
+}
