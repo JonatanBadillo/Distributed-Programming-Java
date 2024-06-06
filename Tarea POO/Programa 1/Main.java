ec77f7bfd9aec46ce12849454463fb31b3a840de
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-05 18:45:38 -0600

    Refactorizar clase Main para eliminar instancia redundante de la clase Punto

---
+++ Tarea POO/Programa 1/Main.java
@@ -0,0 +1,8 @@
+// 1) Escribir un método estático de una clase estática Util calculando el valor de la “función de
+// Ackermann” definida para m ≥ 0 y n ≥ 0 por:
+// • A(m, n) = A(m-1, A(m, n-1)) para m > 0 y n > 0,
+// • A(0, n) = n+1 para n > 0,
+// • A(m, 0) = A(m-1, 1) para m > 0.
+public class Main {
+    
+}
