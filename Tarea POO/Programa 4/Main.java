cd63632918399366da82997f755d7ec148fe4b6e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-05 18:29:09 -0600

    feat: Agregar clase Circulo con métodos cambiaRadio, getCentro y visualiza

--- Tarea POO/Programa 4/Main.java
+++ Tarea POO/Programa 4/Main.java
@@ -26,46 +26,68 @@ class Punto {
 }
 
 // Queremos crear una clase Circulo con los siguientes metodos:
-    // • visualiza que muestra las coordenadas del centro del círculo y su radio.
-    // 1. Define la clase Circulo como una clase derivada de Punto.
-    // 2. Define la clase Circulo como si tuviera un miembro de tipo Punto.
-// En ambos casos escribiremos un pequeño programa poniendo en juego las diferentes
-// funcionalidades de la clase Circulo.
 
-class Circulo{
+// 1. Define la clase Circulo como una clase derivada de Punto.
+class Circulo1 extends Punto{
+    private double radio;
+
+    // • constructor que recibe como argumentos las coordenadas del centro del
+    // círculo y su radio
+    public Circulo1(double x, double y, double radio) {
+        super(x, y);
+        this.radio = radio;
+    }
+
+    // • mueveCentro para modificar las coordenadas del centro del círculo,
+    public void mueveCentro(double dx, double dy) {
+        desplaza(dx, dy);
+    }
+
+    // • cambiaRadio para modificar el radio del círculo,
+    public void cambiaRadio(double nuevoRadio) {
+        this.radio = nuevoRadio;
+    }
+
+    // • getCentro que proporciona como resultado un objeto de tipo Punto
+    // correspondiente al centro del círculo
+    public Punto getCentro() {
+        return this;
+    }
+
+    // • visualiza que muestra las coordenadas del centro del círculo y su radio.
+    public void visualiza() {
+        super.visualiza();
+        System.out.println(" y su radio: " + radio);
+    }
+}
+
+
+// 2. Define la clase Circulo como si tuviera un miembro de tipo Punto.
+class Circulo2 {
     private Punto centro;
     private double radio;
 
-//  • constructor que recibe como argumentos las coordenadas del centro del círculo y su
-//  radio
-    public Circulo(double x, double y, double radio){
+    public Circulo2(double x, double y, double radio) {
         this.centro = new Punto(x, y);
         this.radio = radio;
     }
-// • mueveCentro para modificar las coordenadas del centro del círculo,
-    public void mueveCentro(double dx, double dy){
+
+    public void mueveCentro(double dx, double dy) {
         centro.desplaza(dx, dy);
     }
-// • cambiaRadio para modificar el radio del círculo,
-public void cambiaRadio(double radio){
-    this.radio = radio;
+
+    public void cambiaRadio(double nuevoRadio) {
+        this.radio = nuevoRadio;
+    }
+
+    public Punto getCentro() {
+        return centro;
+    }
+
+    public void visualiza() {
+        System.out.println("Coordenadas del centro del círculo: " + centro.getX() + " " + centro.getY() + " y su radio: " + radio);
+    }
 }
-//  • getCentro que proporciona como resultado un objeto de tipo Punto correspondiente
-//  al centro del círculo
-public Punto getCentro(){
-    return centro;
-}
-// • visualiza que muestra las coordenadas del centro del círculo y su radio.
-public void visualiza(){
-    System.out.println("Coordenadas del centro del círculo : " + centro.getX() + " " + centro.getY());
-    System.out.println("Radio del círculo : " + radio);
-}
-
-
-}
-
-
-
 
 
 public class Main {
