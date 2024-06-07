bb35e09cf2fac6489ac6b8ff4c746753ae8c2b7e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-07 13:09:26 -0600

    Refactorizar clase TestTeam para mejorar la creación y visualización de un equipo de jugadores

--- Mas Ejemplos Java/testteam/src/testteam/TestTeam.java
+++ Mas Ejemplos Java/testteam/src/testteam/TestTeam.java
@@ -1,14 +1,47 @@
 package testteam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestTeam {
 
     public static void main(String[] args) {
-        TeamType team1 = new Team();
-	// Since an object of TeamType has been created, we are only able
-	// to call upon the methods that were made available via the
-	// TeamType interface
+        // Create players
+        Player player1 = new Player() {
+            {
+                setFirstName("John");
+                setLastName("Doe");
+                setPosition("Forward");
+                setStatus(0);
+            }
+        };
 
-        team1.getFullName();
+        Player player2 = new Player() {
+            {
+                setFirstName("Jane");
+                setLastName("Smith");
+                setPosition("Guard");
+                setStatus(1);
+            }
+        };
+
+        // Create a list of players and add the players to the list
+        List<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+
+        // Create a team and set its properties
+        Team team1 = new Team();
+        team1.setPlayers(players);
+        team1.setName("Warriors");
+        team1.setCity("San Francisco");
+
+        // Display team information
+        System.out.println("Team Full Name: " + team1.getFullName());
+        System.out.println("Players:");
+
+        for (Player player : team1.getPlayers()) {
+            System.out.println(player.playerString() + " - Status: " + player.playerStatus());
+        }
     }
-    
 }
