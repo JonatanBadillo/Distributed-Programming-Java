d1ffa239e25c04e5cf9b2b797cce66bdd8e47d97
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-12 10:18:37 -0600

    EscritorMutex

--- Mas Programas Concurrentes/EscritorMutex/EscritorMutex/src/EscritorMutex.java
+++ Mas Programas Concurrentes/EscritorMutex/EscritorMutex/src/EscritorMutex.java
@@ -12,9 +12,12 @@ public class EscritorMutex {
     public static void main(String[] args) {
         // Creo los objetos escritorA y escritorB  de la clase EscritorM
         EscritorM escritorA, escritorB;
+        // Creo mi objeto Impresora
         Impresora impresora = new Impresora();
+        // nuestro objetos de escritor , le agrego como parametros un String y el objeto creado: impresora
         escritorA = new EscritorM("ABC", impresora);
         escritorB = new EscritorM("XYZ", impresora);
+        // Lanzamos nuestros hilos escritorA y escritorB
         escritorA.start();
         escritorB.start();
     }
