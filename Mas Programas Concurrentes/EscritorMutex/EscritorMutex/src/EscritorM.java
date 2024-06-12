63855cab2f532121a28e6a27988861f025434fc7
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-12 17:55:19 -0600

    chore: Agregar lógica de concurrencia en EscritorM.java

--- Mas Programas Concurrentes/EscritorMutex/EscritorMutex/src/EscritorM.java
+++ Mas Programas Concurrentes/EscritorMutex/EscritorMutex/src/EscritorM.java
@@ -3,12 +3,14 @@ package escritormutex;
 public class EscritorM extends Thread {
     private String texto;
     private Impresora imprime;
+    // constructor que recibe como parametro un String y un objeto Impresora
     public EscritorM(String t, Impresora i){
         texto=t;
         imprime=i;
     }
     public void run(){
         for (int i=0; i<10; i++){
+            //
             imprime.imprimir(texto);
             try{sleep((long)(Math.random()*100));}
             catch(InterruptedException e) {}
