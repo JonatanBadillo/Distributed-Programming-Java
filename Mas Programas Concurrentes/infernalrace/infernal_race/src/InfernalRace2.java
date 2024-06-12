6028bc800ef28f9dcd079f2bfef295c64770121e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-12 10:02:59 -0600

    lógica de concurrencia en InfernalRace2.java

--- Mas Programas Concurrentes/infernalrace/infernal_race/src/InfernalRace2.java
+++ Mas Programas Concurrentes/infernalrace/infernal_race/src/InfernalRace2.java
@@ -5,19 +5,26 @@ package infernalrace2;
 
 public class InfernalRace2 {
     public static void main(String[] args) {
+        // Crea dos instancias de la clase Runner con los nombres "A" y "B".
         Runner A = new Runner("A"); Runner B = new Runner("B");
+
+        // Asigna la máxima prioridad (Thread.MAX_PRIORITY) al hilo A y la mínima prioridad (Thread.MIN_PRIORITY) al hilo B.
         A.setPriority(Thread.MAX_PRIORITY);
         B.setPriority(Thread.MIN_PRIORITY);
+
+        // Imprime las prioridades de ambos hilos.
         System.out.println("Thread Runner " + A.name + " has priority = "
                 + A.getPriority());
         System.out.println("Thread Runner " + B.name + " has priority = "
                 + B.getPriority());
+
+        // Inicia ambos hilos.
         A.start(); B.start();
     }
 }
 
 class Runner extends Thread {
-    String name
+    String name;
     public Runner(String name) {
         super(name);
         this.name = name;
