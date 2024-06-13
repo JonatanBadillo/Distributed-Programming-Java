5dea75552a19379017921d6b3b78f88448b3f6b0
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-12 18:00:17 -0600

    feat: Agregar lógica de concurrencia en EscritorM.java

--- Mas Programas Concurrentes/EscritorMutex/EscritorMutex/src/EscritorM.java
+++ Mas Programas Concurrentes/EscritorMutex/EscritorMutex/src/EscritorM.java
@@ -10,11 +10,13 @@ public class EscritorM extends Thread {
     }
     public void run(){
         for (int i=0; i<10; i++){
-            //
+            // Imprime el texto 10 veces utilizando el método imprimir de la clase Impresora
             imprime.imprimir(texto);
+            // Entre cada impresión, el hilo se duerme por un período aleatorio de tiempo (hasta 100 milisegundos).
             try{sleep((long)(Math.random()*100));}
             catch(InterruptedException e) {}
         }
+        // Después de las 10 iteraciones, imprime un mensaje indicando que ha terminado.
         System.out.println("Escritor de " + texto + " ha concluido");
     }
 }
