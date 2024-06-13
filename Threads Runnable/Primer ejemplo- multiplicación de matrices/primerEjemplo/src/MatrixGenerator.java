7611ca356cd702f2a4dce109186ac86a7d401093
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-13 09:31:09 -0600

    chore: Agregar archivos de configuración de proyecto

---
+++ Threads Runnable/Primer ejemplo- multiplicación de matrices/primerEjemplo/src/MatrixGenerator.java
@@ -0,0 +1,17 @@
+// Para implementar este ejemplo, hemos utilizado una clase llamada MatrixGenerator.
+// La usamos para generar matrices aleatorias para multiplicar. Esta clase tiene un método llamado
+// generate() que recibe el número de filas y columnas que deseas en tu matriz como parámetros
+// y genera una matriz con esas dimensiones con números aleatorios double. Este es el código
+// fuente de la clase:
+public class MatrixGenerator {
+    public static double[][] generate (int rows, int columns) {
+        double[][] ret=new double[rows][columns];
+        Random random=new Random();
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<columns; j++) {
+                ret[i][j]=random.nextDouble()*10;
+            }
+        }
+        return ret;
+    }
+}
