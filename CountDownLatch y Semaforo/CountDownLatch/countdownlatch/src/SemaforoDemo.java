eb1d27d35ff1d6bddc6e8aa3bf03762982869249
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-17 10:29:33 -0600

    chore: Actualizar SemaforoDemo.java con mejoras en el bloqueo y espera de hilos

--- CountDownLatch y Semaforo/CountDownLatch/countdownlatch/src/SemaforoDemo.java
+++ CountDownLatch y Semaforo/CountDownLatch/countdownlatch/src/SemaforoDemo.java
@@ -9,7 +9,9 @@ import java.util.concurrent.Semaphore;
 public class SemaforoDemo {
 
     public static void main(String[] args) {
+        // Crea una instancia a Pool
         final Pool pool = new Pool();
+        // Nuestro metodo Run
         Runnable r = new Runnable()
         {
             @Override
@@ -18,6 +20,7 @@ public class SemaforoDemo {
                 String name = Thread.currentThread().getName();
                 try
                 {
+                    // bucle infinito donde muestra el estado del pool del hilo y su accion actual
                     while (true)
                     {
                         String item;
@@ -36,8 +39,9 @@ public class SemaforoDemo {
                 }
             }
         };
-        ExecutorService[] executors =
-                new ExecutorService[Pool.MAX_AVAILABLE + 1];
+        // Se crea un array de ExecutorService con un tamaño de Pool.MAX_AVAILABLE + 1,
+        // lo que significa que habrá un hilo adicional más allá del número máximo de recursos disponibles en el pool.
+        ExecutorService[] executors = new ExecutorService[Pool.MAX_AVAILABLE + 1];
         for (int i = 0; i < executors.length; i++)
         {
             executors[i] = Executors.newSingleThreadExecutor();
@@ -48,12 +52,17 @@ public class SemaforoDemo {
 
 final class Pool
 {
+    // Define un grupo de MAX_AVAILABLE elementos (10 por defecto).
     public static final int MAX_AVAILABLE = 10;
 
+    // Utiliza un Semaphore llamado available con un recuento inicial de MAX_AVAILABLE.
+    // Este semáforo, permite que solo un cierto número de hilos accedan al grupo simultáneamente.
     private final Semaphore available = new Semaphore(MAX_AVAILABLE, true);
 
+    // Almacena elementos en un array de String (items).
     private final String[] items;
 
+    // Mantiene un array de boolean (used) para ver qué elementos están en uso actualmente.
     private final boolean[] used = new boolean[MAX_AVAILABLE];
 
     Pool()
@@ -63,15 +72,24 @@ final class Pool
             items[i] = "I" + i;
     }
 
+
+    // Proporciona métodos getItem() y putItem() para un acceso seguro para hilos al grupo.
+    // Llama a available.acquire(), lo que decrementa el contador del semáforo y bloquea si no hay recursos disponibles.
     String getItem() throws InterruptedException
     {
+        // acquire() decrementa el contador y bloquea el hilo que llama si el contador llega a cero (no hay permisos disponibles).
         available.acquire();
+        // Llama a getNextAvailableItem() para obtener el siguiente recurso disponible.
         return getNextAvailableItem();
     }
 
+
+
     void putItem(String item)
     {
+        // Llama a markAsUnused(item), lo que marca el recurso como no utilizado.
         if (markAsUnused(item))
+        // Llama a available.release() para incrementar el contador del semáforo.
             available.release();
     }
 
