31f939ecedc05c34a02c62fce400a537a9151575
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-17 18:40:16 -0600

    chore: Actualizar configuración de IntelliJ IDEA y eliminar archivos innecesarios

---
+++ KNN/knn/src/Sample.java
@@ -0,0 +1,14 @@
+public abstract class Sample {
+
+	/**
+	 * Method that returns the tag or class of the example
+	 * @return The tag or class of the examples
+	 */
+	public abstract String getTag();
+	
+	/**
+	 * Method that return the values of the attributes of the example as an array of doubles
+	 * @return The values of the attributes of the example
+	 */
+	public abstract double[] getExample();
+}
