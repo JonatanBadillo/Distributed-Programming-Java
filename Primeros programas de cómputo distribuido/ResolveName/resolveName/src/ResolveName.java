06c5c98bafcfff4bb49602684ea1175fb62a7d67
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-20 12:04:22 -0600

    nombres de host a través de InetAddress

--- Primeros programas de cómputo distribuido/ResolveName/resolveName/src/ResolveName.java
+++ Primeros programas de cómputo distribuido/ResolveName/resolveName/src/ResolveName.java
@@ -1,20 +1,31 @@
 /*
-  El objetivo del siguiente código es encontrar la dirección IP correspondiente 
-  al nombre de una máquina pasada como argumento.
+ * Este programa demuestra cómo resolver un nombre de host (dominio) a su correspondiente dirección IP.
+ * Actualmente solo muestra ejemplos para "localhost" y "www.facebook.com".
  */
 package resolvename;
+
 import java.net.*;
 
 public class ResolveName {
-    public static void main(String[] args) {
-        InetAddress adresse;
-        try{
-            adresse=InetAddress.getByName("localhost");
-            System.out.println(adresse.getHostAddress());
-            adresse=InetAddress.getByName("www.facebook.com");
-            System.out.println(adresse.getHostAddress());
-            System.out.println(adresse);
-        }catch(UnknownHostException e) { }
-    }
 
+    public static void main(String[] args) {
+        // Declarar una variable de tipo InetAddress para almacenar la dirección IP resuelta
+        InetAddress address;
+
+        try {
+            // Intentar resolver el nombre de host "localhost" y almacenar la dirección IP
+            address = InetAddress.getByName("localhost");
+            System.out.println("La dirección IP de 'localhost' es: " + address.getHostAddress());
+
+            // Intentar resolver el nombre de host "www.facebook.com" y almacenar la dirección IP
+            address = InetAddress.getByName("www.facebook.com");
+            System.out.println("La dirección IP de 'www.facebook.com' es: " + address.getHostAddress());
+
+            // Imprimir información adicional sobre el objeto InetAddress (podría incluir más que la IP)
+            System.out.println(address);
+        } catch (UnknownHostException e) {
+            // Manejar la excepción si el nombre de host no se puede resolver
+            System.err.println("Error: No se pudo resolver el nombre de host. " + e.getMessage());
+        }
+    }
 }
